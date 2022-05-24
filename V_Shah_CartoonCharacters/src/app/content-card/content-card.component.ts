b09ed57b46737ca530334ddaab745b127b9101c8
VishvaShah01 <W0784026@myscc.ca>
2022-05-24 13:46:37 -0400

    display contentCard using design ideas in .scss files

--- V_Shah_CartoonCharacters/src/app/content-card/content-card.component.ts
+++ V_Shah_CartoonCharacters/src/app/content-card/content-card.component.ts
@@ -4,7 +4,7 @@ import {Content} from '../models/content';
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.css']
+  styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
   @Input() cartoon?: Content;
--- V_Shah_CartoonCharacters/src/app/content-list/content-list.component.ts
+++ V_Shah_CartoonCharacters/src/app/content-list/content-list.component.ts
@@ -4,7 +4,7 @@ import { Content } from '../models/content';
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
-  styleUrls: ['./content-list.component.css']
+  styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
 
