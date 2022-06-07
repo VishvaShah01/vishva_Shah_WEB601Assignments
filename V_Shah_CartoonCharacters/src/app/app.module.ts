02e818bc0aab683066de2ad5d9651e7cfc611877
VishvaShah01 <W0784026@myscc.ca>
2022-06-07 13:58:33 -0400

    created directive and modify properties

--- V_Shah_CartoonCharacters/src/app/app.module.ts
+++ V_Shah_CartoonCharacters/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { CartoonsPipe } from './cartoons.pipe';
+import { HighlightImportantDataDirective } from './highlight-important-data.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentCardComponent,
-    CartoonsPipe
+    CartoonsPipe,
+    HighlightImportantDataDirective
   ],
   imports: [
     BrowserModule,
