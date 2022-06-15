daaa10ab54681e3685660dc380df47f4adbd278d
VishvaShah01 <W0784026@myscc.ca>
2022-06-15 12:21:36 -0400

    move content array - mock

---
+++ V_Shah_CartoonCharacters/src/app/data/mock-cartoons.ts
@@ -0,0 +1,64 @@
+import {Content} from "../models/content";
+export const cartooncharacters:Content[] = [{
+    id: 0,
+      title: 'Shinchan',
+      body: ' Shin-chan follows the journey of five-year-old, Shinnosuke "Shin" Nohara and his family, his pet dog Shiro, neighbours, and friends in Kasukabe, Japan.',
+      author: 'Yoshito Usui',
+      // imagelink: '',
+      type: 'Comedy',
+      hashtag: ['Sinchan Nohara']
+    },
+    {
+      id: 1,
+      title: 'Mickey Mouse',
+      body: 'Mickey Mouse is an American animated cartoon character co-created in 1928 by Walt Disney, who originally voiced the character, and Ub Iwerks.',
+      author: 'Walt Disney',
+      imagelink: 'https://vignette.wikia.nocookie.net/the-disney-roleplay/images/d/d4/Mickey-mouse-659.jpg/revision/latest/scale-to-width-down/2000?cb=20140917132903',
+      type: 'Comedy',
+      hashtag: ['Mickey Mouse']
+    },
+    {
+      id: 2,
+      title: 'Chotta Bheem',
+      body: "Chhota Bheem ('Little Bheem') is an Indian animated comedy adventure television series, created by Green Gold Animations based in Hyderabad. Bheem is a brave, strong and intelligent young boy. He often manages to solve everyone's problems which endears him to the townspeople of Dholakpur.",
+      author: 'Rajiv Chilaka',
+      imagelink: 'https://www.asianpaints.com/content/dam/asianpaints/website/shop/products/chhota-bheem-good-luck-wall-sticker-a.jpg.image.114.87.medium.jpg',
+      type: 'Comedy',
+      hashtag: ['Bheemmm']
+    },
+    {
+      id: 3,
+      title: 'Thomas and Friends',
+      body: "Thomas & Friends (originally known as Thomas the Tank Engine & Friends and later Thomas the Tank Engine and Thomas & Friends: Big World! Big Adventures!) is a British children's television series created by Reverend W. Awdry and Britt Allcroft that aired across 24 series between 1984 to 2021.",
+      author: 'Britt Allcroft',
+      imagelink: 'https://2.bp.blogspot.com/-ZI0ab75-9yM/WpW3wO_gM2I/AAAAAAAA4AQ/ct1KqUz-A4ULjo3QjbSXqVReV8Y84wDjQCLcBGAs/s1600/thomas-and-friends-season-21-keyart_landscape_no-logo-nickelodeon-usa-press-nick-jr.jpg',
+      type: 'Child',
+      hashtag: ['Thomas']
+    },
+    {
+      id: 4,
+      title: 'Tom & Jerry',
+      body: 'Tom and Jerry is an American animated media franchise and series of comedy short films created in 1940 by William Hanna and Joseph Barbera. Best known for its 161 theatrical short films by Metro-Goldwyn-Mayer, the series centers on the rivalry between the titular characters of a cat named Tom and a mouse named Jerry. Many shorts also feature several recurring characters.',
+      author: 'William Hanna',
+      imagelink:'http://3.bp.blogspot.com/-GXjhWVhovBM/TwQfW6PNBeI/AAAAAAAAIbs/iFRur1qJgrA/s1600/Tom-and-Jerry+12.jpg',
+      type: 'Child',
+      hashtag: ['Cat and Mouse']
+    },
+    {
+      id: 5,
+      title: 'Mr Bean',
+      body: 'Mr. Bean is a British sitcom created by Rowan Atkinson and Richard Curtis, produced by Tiger Aspect and starring Atkinson as the title character.',
+      author: 'Rowan Atkinson and Richard Curtis',
+      imagelink:'https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/05/BeaNFT.jpg?q=50&fit=contain&w=750&h=375&dpr=1.5',
+      type: '',
+      hashtag: ['Mr Bean'] 
+    },
+    {
+      id: 6,
+      title: 'Doraemon',
+      body: 'Doraemon is a Japanese manga series written and illustrated by Fujiko F. Fujio.',
+      author: ' Fujiko F. Fujio',
+      imagelink:'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/doremon-anwar.jpg',
+      type: '',
+      hashtag: ['Doraemon'] 
+}];
