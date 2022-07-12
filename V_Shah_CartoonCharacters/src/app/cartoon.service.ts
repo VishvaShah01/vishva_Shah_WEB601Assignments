8d5bed50b01d610ec95fed81f697c2c4245eda6d
VishvaShah01 <W0784026@myscc.ca>
2022-07-12 16:42:34 -0400

    Perform CRUD and changes in cartoon service file

--- V_Shah_CartoonCharacters/src/app/cartoon.service.ts
+++ V_Shah_CartoonCharacters/src/app/cartoon.service.ts
@@ -2,65 +2,80 @@ import { Injectable } from '@angular/core';
 import {cartooncharacters, DEFAULTCARTOONCHARACTERS} from "../app/data/mock-cartoons";
 import {Content} from "../app/models/content";
 import {Observable, of} from "rxjs";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartoonService {
 
-  constructor() { }
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type':
+        'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
 
   //getting file of mock-cartoons
   //A method that will return the content array from the file in your data folder
   getCartoons(): Observable<Content[]> {
-    return of(cartooncharacters);
+    //return of(cartooncharacters);
+    return this.http.get<Content[]>("/api/cartoonChar");
   }
 
   //A method that accepts a number and returns the content item in the array that contains the same id as the number parameter
   //insert data
   getCartoonData(id: Number): Observable<Content> {
-    for (var i = 0; i < cartooncharacters.length; i++) {
+    /* for (var i = 0; i < cartooncharacters.length; i++) {
       if (cartooncharacters[i].id === id) {
         console.log(cartooncharacters[i])
         return of(cartooncharacters[i]);
       }
   
-    }
-    console.log(cartooncharacters)
+    } */
+    /* console.log(cartooncharacters)
     //return of(DEFAULTCARTOONCHARACTERS);
-    return of(cartooncharacters[i]);
+    return of(cartooncharacters[i]); */
+
+    console.log("Now getting it from the server!");
+    return this.http.get<Content>("/api/cartoonChar/" + id);
   }
 
   //A method that accepts a Content item as an input, adds the item to the array, and returns the array after the item is added
   //update data 
-  addCartoonData(newCartoon: Content): Observable<Content[]> {
-    var printCartoon = cartooncharacters.filter((cartoon) => {
+  addCartoonData(newCartoon: Content): Observable<Content> {
+    /* var printCartoon = cartooncharacters.filter((cartoon) => {
       return cartoon.id == newCartoon.id;
     });
     cartooncharacters.push(newCartoon);
-    return of(cartooncharacters);
+    return of(cartooncharacters); */
+
+    return this.http.post<Content>("/api/cartoonChar", newCartoon, this.httpOptions)
   }
 
   //A method that accepts a Content item as an input, updates an existing item in the array that has the same id as the input parameter, and returns the array after the item is updated
   //delete data
   updateCartoonData(newCartoon: Content): Observable<Content[]> {
-    var  updateData = cartooncharacters.forEach(cartoons =>{
+    /* var  updateData = cartooncharacters.forEach(cartoons =>{
       if( cartoons.id){
         cartoons.id === newCartoon.id ? { ...cartoons, ...newCartoon } : cartoons
       }
     });
-    return of(cartooncharacters);
+    return of(cartooncharacters); */
+    return this.http.put<any>("api/cartoonChar", newCartoon, this.httpOptions);
   }
 
   //A method that accepts a number, removes the item from the array that has the same id as the number parameter, and returns the Content item that was removed
-  deleteCartoonData(id: Number): Observable<Content[]> {
+  /* deleteCartoonData(id: Number): Observable<Content[]> {
     var deletedData = cartooncharacters.filter(function (newCartoon) {
       if(newCartoon.id != id) {
         console.log(cartooncharacters);
       }
     });
     return of(deletedData);
-  }
+  } */
 
 
 }
